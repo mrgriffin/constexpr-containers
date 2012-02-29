fc5098ca7b42538c44fe8fe0dd6f47c3f1c10d61
Martin Griffin <martinrgriffin@gmail.com>
2012-02-29 14:38:15 +0000

    Created cexpr::detail::basic_list::const_iterator. Adapted set/insert/erase constructors to use iterator ranges.

--- cexpr/detail/list.hpp
+++ cexpr/detail/list.hpp
@@ -8,22 +8,82 @@
 
 #include <cstddef>
 #include <initializer_list>
+#include <iterator>
 
 namespace cexpr {
 namespace detail {
 
-constexpr struct set_t { constexpr set_t() {} } SET;
-constexpr struct insert_t { constexpr insert_t() {} } INSERT;
+template<typename T, std::size_t N>
+class basic_list;
+
+template<typename T, std::size_t N>
+class basic_list_iterator;
+
+template<typename T, std::size_t N>
+constexpr bool operator==(basic_list_iterator<T, N> lhs, basic_list_iterator<T, N> rhs);
+
+template<typename T, std::size_t N>
+constexpr bool operator!=(basic_list_iterator<T, N> lhs, basic_list_iterator<T, N> rhs);
+
+//! \c constexpr \c std::iterator -like object for iterating through basic_list.
+template<typename T, std::size_t N>
+class basic_list_iterator {
+public:
+	constexpr basic_list_iterator(basic_list<T, N> const& list)
+		: list(list)
+		, position(0)
+		{}
+
+	constexpr basic_list_iterator(basic_list<T, N> const& list, typename basic_list<T, N>::size_type position)
+		: list(list)
+		, position(position)
+		{}
+
+	//! Advances forwards \p pos elements.
+	constexpr basic_list_iterator operator+(typename basic_list<T, N>::size_type pos) {
+		return { list, position + pos };
+	}
+
+	//! Moves backwards \p pos elements.
+	constexpr basic_list_iterator operator-(typename basic_list<T, N>::size_type pos) {
+		return { list, position - pos };
+	}
+
+	friend bool operator==<>(basic_list_iterator, basic_list_iterator);
+	friend bool operator!=<>(basic_list_iterator, basic_list_iterator);
+
+	//! Returns the current value.
+	//! \warning Needs optimization.
+	constexpr T operator*() {
+		return list[position];
+	}
+
+private:
+	basic_list<T, N> const& list;
+	typename basic_list<T, N>::size_type position;
+};
+
+template<typename T, std::size_t N>
+constexpr bool operator==(basic_list_iterator<T, N> lhs, basic_list_iterator<T, N> rhs) {
+	return &lhs.list == &rhs.list && lhs.position == rhs.position;
+}
+
+template<typename T, std::size_t N>
+constexpr bool operator!=(basic_list_iterator<T, N> lhs, basic_list_iterator<T, N> rhs) {
+	return !(lhs == rhs);
+}
 
 //! \c constexpr list containing exactly \p N elements of type \p T.
 template<typename T, std::size_t N>
 class basic_list {
 friend class basic_list<T, N+1>;
 friend class basic_list<T, N-1>;
+friend class basic_list_iterator<T, N>;
 
 public:
 	typedef T value_type;
 	typedef std::size_t size_type;
+	typedef basic_list_iterator<T, N> const_iterator;
 
 	//! Constructs a list containing up to \p N elements.
 	//! \details The unused elements are default-constructed.
@@ -79,28 +139,57 @@ public:
 
 	//! Overwrites the element at \p pos.
 	constexpr basic_list set(size_type pos, T const& value) {
-		return { *this, 0, pos, value, SET };
+		return { cbegin(), cbegin() + pos, value, false, cbegin() + pos + 1, cend(), T() };
 	}
 
 	//! Inserts an element at \p pos.
 	constexpr basic_list insert(size_type pos, T const& value) {
-		return { *this, 0, pos, value, INSERT };
+		// TODO: Replace T() with something else.
+		// HINT: T() will never be used.
+		return { cbegin(), cbegin() + pos, value, false, cbegin() + pos, cend(), T() };
 	}
 
 	//! Inserts a series of elements at \p pos.
-	constexpr basic_list insert(size_type pos, std::initializer_list<T> values);
+	constexpr basic_list insert(size_type pos, std::initializer_list<T> values) {
+		// TODO: Replace T() with something else.
+		// HINT: T() will never be used.
+		return { cbegin(), cbegin() + pos, values.begin(), values.end(), cbegin() + pos, cend(), T() };
+	}
 
 	//! Removes the element at \p pos.
 	constexpr basic_list erase(size_type pos) {
-		return { *this, 0, pos };
+		return erase(pos, pos + 1);
 	}
 
 	//! Removes the elements in the range [ \p first, \p last ).
-	constexpr basic_list erase(size_type first, size_type last);
+	constexpr basic_list erase(size_type first, size_type last) {
+		// TODO: Replace T() with something else.
+		return { cbegin(), cbegin() + first, cbegin() + last, cend(), T() };
+	}
 
 	//! Returns the number of elements.
 	constexpr size_type size() {
 		return N;
+	}
+
+	//! Returns an iterator to the first element.
+	constexpr const_iterator begin() {
+		return cbegin();
+	}
+
+	//! Returns an iterator to the first element.
+	constexpr const_iterator cbegin() {
+		return const_iterator(*this);
+	}
+
+	//! Returns an iterator to one-past the last element.
+	constexpr const_iterator end() {
+		return cend();
+	}
+
+	//! Returns an iterator to one-past the last element.
+	constexpr const_iterator cend() {
+		return const_iterator(*this, N);
 	}
 
 private:
@@ -112,29 +201,44 @@ private:
 		, tail(first + 1, last, value)
 		{}
 
-	//! Constructs a copy of \p that where the value at \p pos is equal to \p value.
-	template<std::size_t M>
-	constexpr basic_list(basic_list<T, M> const& that, size_type count, size_type pos, T const& value, set_t)
-		: head(count != pos ? that[count] : value)
-		, tail(that, count + 1, pos, value, SET)
+	template<class InputIterator1, class InputIterator2>
+	constexpr basic_list(
+		InputIterator1 first1, InputIterator1 last1,
+		InputIterator2 first2, InputIterator2 last2,
+		T const& value)
+		: head(first1 != last1 ? *first1 : first2 != last2 ? *first2 : value)
+		, tail(
+			first1 != last1 ? first1 + 1 : first1, last1,
+			first1 == last1 && first2 != last2 ? first2 + 1 : first2, last2,
+			value)
 		{}
 
-	//! Constructs a copy of \p that where the value at \p pos is equal to \p value and ( \p pos, \p N ] are as in \p that ( \p pos + 1, \p N ].
-	//! \warning \p M must be >= \p N.
-	//! \warning \p count must be < \c (size_type)-1.
-	template<std::size_t M>
-	constexpr basic_list(basic_list<T, M> const& that, size_type count, size_type pos, T const& value, insert_t)
-		: head(count != pos ? that[count] : value)
-		, tail(that, count != pos ? count + 1 : count, count != pos ? pos : -1, value, INSERT)
+	template<class InputIterator1, class InputIterator2>
+	constexpr basic_list(
+		InputIterator1 first1, InputIterator1 last1,
+		T const& insert, bool inserted,
+		InputIterator2 first2, InputIterator2 last2,
+		T const& value)
+		: head(first1 != last1 ? *first1 : !inserted ? insert : first2 != last2 ? *first2 : value)
+		, tail(
+			first1 != last1 ? first1 + 1 : first1, last1,
+			insert, first1 == last1,
+			first1 == last1 && inserted && first2 != last2 ? first2 + 1 : first2, last2,
+			value)
 		{}
 
-	//! Constructs a copy of \p that where the values at ( \p pos, \p N ] are as in \p that ( \p pos + 1, \p N ].
-	//! \details The value at \p N-1 is equal to that at \p N-2.
-	//! \warning \p M must be >= \p N.
-	template<std::size_t M>
-	constexpr basic_list(basic_list<T, M> const& that, size_type count, size_type pos)
-		: head(count >= M ? that[M - 1] : count != pos ? that[count] : that[count + 1])
-		, tail(that, count != pos ? count + 1 : count + 2, pos)
+	template<class InputIterator1, class InputIterator2, class InputIterator3>
+	constexpr basic_list(
+		InputIterator1 first1, InputIterator1 last1,
+		InputIterator2 first2, InputIterator2 last2,
+		InputIterator3 first3, InputIterator3 last3,
+		T const& value)
+		: head(first1 != last1 ? *first1 : first2 != last2 ? *first2 : first3 != last3 ? *first3 : value)
+		, tail(
+			first1 != last1 ? first1 + 1 : first1, last1,
+			first1 == last1 && first2 != last2 ? first2 + 1 : first2, last2,
+			first1 == last1 && first2 == last2 && first3 != last3 ? first3 + 1 : first3, last3,
+			value)
 		{}
 
 	T head;
@@ -148,6 +252,7 @@ friend class basic_list<T, 1>;
 public:
 	typedef T value_type;
 	typedef std::size_t size_type;
+	typedef basic_list_iterator<T, 0> const_iterator;
 
 	constexpr basic_list() {}
 	constexpr explicit basic_list(T const& value) {}
@@ -179,20 +284,26 @@ public:
 
 	constexpr size_type size() { return 0; }
 
+	constexpr const_iterator begin() { return cbegin(); }
+	constexpr const_iterator cbegin() { return const_iterator(*this); }
+
+	constexpr const_iterator end() { return cend(); }
+	constexpr const_iterator cend() { return const_iterator(*this); }
+
 private:
 	constexpr int fail() { return throw "attempt to access outside of array", 0; }
 
 	template<class InputIterator>
 	constexpr basic_list(InputIterator first, InputIterator last, T const& value) {}
 
-	template<std::size_t M>
-	constexpr basic_list(basic_list<T, M> const& that, size_type count, size_type pos, T const& value, set_t) {}
+	template<class InputIterator1, class InputIterator2>
+	constexpr basic_list(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, InputIterator2 last2, T const& value) {}
+	
+	template<class InputIterator1, class InputIterator2>
+	constexpr basic_list(InputIterator1 first1, InputIterator1 last1, T const& insert, bool inserted, InputIterator2 first2, InputIterator2 last2, T const& value) {}
 
-	template<std::size_t M>
-	constexpr basic_list(basic_list<T, M> const& that, size_type count, size_type pos, T const& value, insert_t) {}
-
-	template<std::size_t M>
-	constexpr basic_list(basic_list<T, M> const& that, size_type count, size_type pos) {}
+	template<class InputIterator1, class InputIterator2, class InputIterator3>
+	constexpr basic_list(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, InputIterator2 last2, InputIterator3 first3, InputIterator3 last3, T const& value) {}
 };
 
 }
--- test/detail/list.cpp
+++ test/detail/list.cpp
@@ -17,3 +17,16 @@ static_assert(c[0] == 0, "!=0");
 static_assert(c[1] == 1, "!=1");
 static_assert(c[2] == 3, "!=3");
 static_assert(c[3] == 3, "!=3");
+
+constexpr auto d = c.erase(0, 1);
+static_assert(d[0] == 1, "!=1");
+static_assert(d[1] == 3, "!=3");
+
+constexpr auto it1 = d.cbegin();
+constexpr auto end = d.cend();
+
+constexpr auto e = d.insert(1, { 2, 3 });
+static_assert(e[0] == 1, "!=1");
+static_assert(e[1] == 2, "!=2");
+static_assert(e[2] == 3, "!=3");
+static_assert(e[3] == 3, "!=3");
