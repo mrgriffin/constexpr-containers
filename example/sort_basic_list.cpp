989e368682c4dc8bcfe033867168984a8e5c4428
Martin Griffin <martinrgriffin@gmail.com>
2012-04-13 08:32:35 +0100

    Simplified the merge sort logic.

--- example/sort_basic_list.cpp
+++ example/sort_basic_list.cpp
@@ -12,41 +12,16 @@ using cexpr::detail::basic_list;
 // TODO: Use a comparator.
 
 template<typename T, size_t N, size_t M>
-constexpr basic_list<T, N+M> merge(basic_list<T, N> list1, basic_list<T, M> list2);
-
-template<typename T, size_t N, size_t M>
-struct merger {
-	constexpr basic_list<T, N+M> operator()(basic_list<T, N> list1, basic_list<T, M> list2) {
-		return list1[0] <= list2[0]
-			? basic_list<T, N+M>(merge(basic_list<T, N-1>(list1.begin() + 1, list1.end()), list2)).insert(0, list1[0])
-			: basic_list<T, N+M>(merge(list1, basic_list<T, M-1>(list2.begin() + 1, list2.end()))).insert(0, list2[0]);
-	}
-};
-
-template<typename T, size_t N>
-struct merger<T, N, 0> {
-	constexpr basic_list<T, N> operator()(basic_list<T, N> list1, basic_list<T, 0> list2) {
-		return list1;
-	}
-};
-
-template<typename T, size_t M>
-struct merger<T, 0, M> {
-	constexpr basic_list<T, M> operator()(basic_list<T, 0> list1, basic_list<T, M> list2) {
-		return list2;
-	}
-};
-
-template<typename T>
-struct merger<T, 0, 0> {
-	constexpr basic_list<T, 0> operator()(basic_list<T, 0> list1, basic_list<T, 0> list2) {
-		return {};
-	}
-};
-
-template<typename T, size_t N, size_t M>
 constexpr basic_list<T, N+M> merge(basic_list<T, N> list1, basic_list<T, M> list2) {
-	return merger<T, N, M>()(list1, list2);
+	return N > 0 && M > 0
+		? list1[0] <= list2[0]
+			? basic_list<T, N+M>(merge(basic_list<T, (N > 0 ? N-1 : 0)>(list1.begin() + 1, list1.end()), list2)).insert(0, list1[0])
+			: basic_list<T, N+M>(merge(list1, basic_list<T, (M > 0 ? M-1 : 0)>(list2.begin() + 1, list2.end()))).insert(0, list2[0])
+		: N > 0
+			? basic_list<T, N+M>(list1) // HINT: N+M == N
+			: M > 0
+				? basic_list<T, N+M>(list2) // HINT: N+M == M
+				: basic_list<T, N+M>(); // HINT: N+M == 0
 }
 
 template<typename T, size_t N>
@@ -58,23 +33,11 @@ constexpr basic_list<T, N+M> sort(basic_list<T, N> list1, basic_list<T, M> list2
 }
 
 template<typename T, size_t N>
-struct sorter {
-	constexpr basic_list<T, N> operator()(basic_list<T, N> const& list) {
-		return sort(basic_list<T, N/2>(list.begin(), list.begin() + N/2),
-			    basic_list<T, (N+1)/2>(list.begin() + N/2, list.end()));
-	}
-};
-
-template<typename T>
-struct sorter<T, 1> {
-	constexpr basic_list<T, 1> operator()(basic_list<T, 1> const& list) {
-		return list;
-	}
-};
-
-template<typename T, size_t N>
 constexpr basic_list<T, N> sort(basic_list<T, N> const& list) {
-	return sorter<T, N>()(list);
+	return N == 1
+		? list
+		: sort(basic_list<T, N/2>(list.begin(), list.begin() + N/2),
+		       basic_list<T, (N+1)/2>(list.begin() + N/2, list.end()));
 }
 
 int main() {
